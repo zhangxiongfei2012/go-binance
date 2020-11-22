19a6905378ab5529dd727c34e191bfaa37277bfa
zhangxiongfei <>
2020-11-22 22:34:17 +0800

    modify test net url

--- client.go
+++ client.go
@@ -165,7 +165,7 @@ func NewClient(apiKey, secretKey string) *Client {
 	return &Client{
 		APIKey:     apiKey,
 		SecretKey:  secretKey,
-		BaseURL:    "https://api.binance.com",
+		BaseURL:    "https://testnet.binance.vision",
 		UserAgent:  "Binance/golang",
 		HTTPClient: http.DefaultClient,
 		Logger:     log.New(os.Stderr, "Binance-golang ", log.LstdFlags),
